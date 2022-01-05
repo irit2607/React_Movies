52d09eae2b9fa845f87e8a9ef15c12d827ca1d7e
Irit Kushwaha <iritkushwaha2607@gmail.com>
2022-01-05 18:32:57 +0530

    movies-data

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -1,36 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
-  fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "imdb8.p.rapidapi.com",
-		"x-rapidapi-key": "65025be1d9msh45ba599bfb12f6bp12d387jsn376593a62ebc"
-	}
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => {
-	console.error(err);
-});
+
+  const[movie, setMovie] = useState(null);
+
+useEffect(()=>{
+  getData();
+
+  async function getData(){
+    const response = await fetch("https://www.omdbapi.com/?s=star%20wars&apikey=55ecb700")
+   
+    const data = await response.json();
+    console.log(data)
+    // storing the data into movies variable
+    setMovie(data);
+    
+  }
+},[]);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3>Movies</h3>
+    {movie && (
+      movie.map((movies,index) => {
+      <div key={index}>
+        <p>{movies.Title}</p>
+      </div>
+    })
+    )}
+    </>
   );
 }
 
