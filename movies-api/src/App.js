052d82fc1f213a97d3a1b9be6309c2db636a9273
Irit Kushwaha <iritkushwaha2607@gmail.com>
2022-01-05 20:50:07 +0530

    font-awe

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -1,32 +1,59 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios'
+import { FaHeart } from "react-icons/fa";
+
 function App() {
 
-  const[movie, setMovie] = useState(null);
+  const myStyle = {
+    fontSize : "20px",
+    padding: "30px",
+    fontFamily: "'Luxurious Roman', cursive" 
+  };
 
-useEffect(()=>{
-  getData();
+  const [movie, setMovie] = useState([]);
+  var shows = [];
+  // useEffect(()=>{
+  //   getData();
 
-  async function getData(){
-    const response = await fetch("https://www.omdbapi.com/?s=star%20wars&apikey=55ecb700")
-   
-    const data = await response.json();
-    console.log(data)
-    // storing the data into movies variable
-    setMovie(data);
-    
+  //   async function getData(){
+  //     const response = await fetch("https://www.omdbapi.com/?s=star%20wars&apikey=55ecb700")
+
+  //     const data = await response.json();
+  //     console.log(data)
+  //     // storing the data into movies variable
+  //     setMovie(data);
+
+  //   }
+  // },[]);
+
+  useEffect(() => {
+    item();
+  }, []);
+
+  const item = async () => {
+    const result = await axios.get("https://www.anapioficeandfire.com/api/books")
+    shows = result.data;
+    setMovie(shows);
+    console.log(shows);
   }
-},[]);
- 
+
   return (
     <>
-    <h3>Movies</h3>
-    {movie && (
-      movie.map((movies,index) => {
-      <div key={index}>
-        <p>{movies.Title}</p>
+      <div style={myStyle}>
+        <h2>Movies</h2>
+        {movie.map((i, index) => {
+          return (
+            <>
+              <div key={index}>
+              
+              <div style={{cursor : "pointer"}}><FaHeart /><p >{i.name}</p>
+              </div>
+              </div>
+            </>
+          )
+        })}
+
       </div>
-    })
-    )}
     </>
   );
 }
