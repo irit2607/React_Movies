66a703198a36c1ab582774fd83fa8c065caf06f3
Irit Kushwaha <iritkushwaha2607@gmail.com>
2022-01-06 19:46:19 +0530

    selected data

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -1,30 +1,20 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios'
 import { FaHeart } from "react-icons/fa";
+import './App.css';
 
 function App() {
 
   const myStyle = {
-    fontSize : "20px",
+    fontSize: "20px",
     padding: "30px",
-    fontFamily: "'Luxurious Roman', cursive" 
+    fontFamily: "'Luxurious Roman', cursive"
   };
 
   const [movie, setMovie] = useState([]);
+  const [data1, setData1] = useState([]);
   var shows = [];
-  // useEffect(()=>{
-  //   getData();
-
-  //   async function getData(){
-  //     const response = await fetch("https://www.omdbapi.com/?s=star%20wars&apikey=55ecb700")
-
-  //     const data = await response.json();
-  //     console.log(data)
-  //     // storing the data into movies variable
-  //     setMovie(data);
-
-  //   }
-  // },[]);
+  let isValid = false;
 
   useEffect(() => {
     item();
@@ -37,23 +27,60 @@ function App() {
     console.log(shows);
   }
 
+  const handleClick = async (name, index) => {
+    setData1([...data1, name]);
+    console.log(data1)
+  }
+
+
   return (
     <>
-      <div style={myStyle}>
+      <div className="App">
+        <table>
+          <tr>
+            <th>Movies</th>
+            <th>Selected Movies</th>
+          </tr>
+          <tr>
+            <td>
+              {movie.map((i, index) => {
+                return (
+                  <div key={index}>
+                    <div style={{ cursor: "pointer" }}><FaHeart onClick={() => handleClick(i.name, index)} /><p >{i.name}</p></div>
+                  </div>
+                )
+              })}
+            </td>
+
+            <td>
+              {data1.map((demo) => {
+                return (
+                  <>
+                    <div>{data1[data1.indexOf(demo)]}</div>
+                  </>
+                )
+              })}
+            </td>
+          </tr>
+
+
+        </table>
+      </div>
+
+      {/* <div style={myStyle}>
         <h2>Movies</h2>
         {movie.map((i, index) => {
           return (
             <>
               <div key={index}>
-              
-              <div style={{cursor : "pointer"}}><FaHeart /><p >{i.name}</p>
-              </div>
+                <div style={{ cursor: "pointer" }}><FaHeart /><p >{i.name}</p>
+                </div>
               </div>
             </>
           )
         })}
 
-      </div>
+      </div> */}
     </>
   );
 }
