40dc23fd8f8e5a227616a4212c5d4deb521a6d10
Irit Kushwaha <iritkushwaha2607@gmail.com>
2022-01-07 12:02:59 +0530

    delete func added

--- movies-api/src/App.js
+++ movies-api/src/App.js
@@ -6,15 +6,19 @@ import './App.css';
 function App() {
 
   const myStyle = {
-    fontSize: "20px",
-    padding: "30px",
-    fontFamily: "'Luxurious Roman', cursive"
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "15px",
+    padding: "8px 30px",
+    borderRadius: "5px",
+    margin: "10px 0px",
+    cursor: "pointer",
+    alignItems: "flex-start"
   };
 
   const [movie, setMovie] = useState([]);
   const [data1, setData1] = useState([]);
   var shows = [];
-  let isValid = false;
 
   useEffect(() => {
     item();
@@ -27,60 +31,66 @@ function App() {
     console.log(shows);
   }
 
-  const handleClick = async (name, index) => {
+  const handleClick = (name, index) => {
     setData1([...data1, name]);
-    console.log(data1)
+
   }
 
+  const onDelete = (e) => {
+    console.log(e);
+    const del = data1.filter((item) => (item !== e))         // 1. data = 1
+    setData1(del);
+    console.log(data1);
+  }
 
   return (
     <>
       <div className="App">
         <table>
           <tr>
-            <th>Movies</th>
-            <th>Selected Movies</th>
+            <th style={{ paddingLeft: "12em", fontSize: "20px", }}>Movies</th>
+            <th style={{ paddingLeft: "15em", fontSize: "20px" }}>Selected Movies</th>
           </tr>
           <tr>
-            <td>
+            <td style={{ paddingLeft: "15em" }}>
               {movie.map((i, index) => {
                 return (
                   <div key={index}>
-                    <div style={{ cursor: "pointer" }}><FaHeart onClick={() => handleClick(i.name, index)} /><p >{i.name}</p></div>
+                    <div style={{ cursor: "pointer" }}>
+                      <p style={{ fontSize: "20px", alignItems: "flex-start" }}>
+                        {i.name}</p>
+                      <td>
+                        <FaHeart style={{ fontSize: "25px" }} onClick={() => handleClick(i.name, index)} />
+                      </td>
+                      <hr></hr>
+
+                    </div>
                   </div>
                 )
               })}
             </td>
+            <div style={{ marginTop: "-0.5em" }}> <td style={{ paddingLeft: "16em", fontSize: "20px" }}>
+              {data1.map((demo) => {                                       // demo is a object in which map func searially executing the object i.e each single data  
+                return (                     // data1[data1.indexOf(demo)]
+                  <>                                                                       
+                    {data1.length === 0 ? "No selected value" : <p>{demo}</p>}                   
 
-            <td>
-              {data1.map((demo) => {
-                return (
-                  <>
-                    <div>{data1[data1.indexOf(demo)]}</div>
+                    <button className="btn btn-sm btn-danger" onClick={() => onDelete(demo)} style={myStyle}>
+                      DELETE
+                    </button><br />
                   </>
                 )
               })}
-            </td>
+            </td></div>
+
           </tr>
 
 
         </table>
+
       </div>
 
-      {/* <div style={myStyle}>
-        <h2>Movies</h2>
-        {movie.map((i, index) => {
-          return (
-            <>
-              <div key={index}>
-                <div style={{ cursor: "pointer" }}><FaHeart /><p >{i.name}</p>
-                </div>
-              </div>
-            </>
-          )
-        })}
 
-      </div> */}
     </>
   );
 }
